a136ba48038a6b6313fe210810445c9f021eb8cd
rahul <rahulraazrs123@gmail.com>
2025-08-02 19:19:39 +0545

    donet

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  'recycle_db',
+  'ecosajha',
   'postgres',
-  '33533',
+  'admin123',
   {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
